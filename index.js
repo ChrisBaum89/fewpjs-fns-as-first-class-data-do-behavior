9193f0db1c49d122e2f3b4b90a1e6fa07bbfe42a
Chris Baum <Christopher.Baum@valpo.edu>
2021-12-13 22:29:06 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let splitTime = time.split(':')
+  let parseTime = parseInt(splitTime)
+  if (parseTime < 12 && parseTime >= 0) {
+    return "Good Morning";
+  }
+  else if (parseTime >= 12 && parseTime < 17){
+    return "Good Afternoon";
+  }
+  else if (parseTime >= 17 && parseTime < 24) {
+    return "Good Evening";
+  }
+  else {
+    return "Error";
+  }
+}
+
+function displayMessage(text) {
+  document.getElementById("greeting").textContent = text
+}
